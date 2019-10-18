74df1a527f07e9a13d7bc8a67c7d1e947661e64a
Nathan Shanners <nathan.shanners@gmail.com>
2019-10-18 23:12:48 +0100

    Added logger and multiple controls for an application loop.  Requires update to tests to provide logger name.

--- common_config_ctrl.py
+++ common_config_ctrl.py
@@ -7,15 +7,19 @@
 
 
 # <editor-fold desc="Function to open the requested config file">
-def config_open_file(config_filename):
+def config_open_file(logger_name, config_filename):
     # Import Control
     import os.path
     import configparser
+    import logging
+
+    # Function logger (handlers typically picked up from parent)
+    fcn_logger = logging.getLogger(logger_name + '.config_open_file')
 
     #  Define variables
     cwd = os.getcwd()
-    if cwd.find('\\tests'):
-        print('Replacing tests in file path, assuming in test mode')
+    if '\\tests' in cwd:
+        fcn_logger.info('Replacing tests in file path, assuming in test mode')
     cwd = cwd.replace('\\tests', '')  # Replace tests folder (used for py.test)
     config_file_path = cwd + '\\' + config_filename
     # Read config file & parse parameters
@@ -23,7 +27,7 @@ def config_open_file(config_filename):
         config = configparser.ConfigParser()
         config.read(config_file_path)
     else:  # Config file is missing
-        print('Config file is missing %s' % config_file_path)
+        fcn_logger.error('Config file is missing %s' % config_file_path)
         config = 'Error'
 
     # Return output from function

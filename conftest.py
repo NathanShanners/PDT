925b32ffe6ebea4bb584388663965c4ac3be47e6
Nathan Shanners <nathan.shanners@gmail.com>
2019-10-18 10:56:43 +0100

    Added header and editor sections for test configuration code.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,26 @@
+# <editor-fold desc="File Header">
+# Copyright   : Copyright information (if applicable)
+# Author      : Author Name
+# Date        : 18/10/2019
+# Description : This python file contains all classes, functions and scripts related to configuration of testing
+# </editor-fold>
+
+
+# <editor-fold desc="Import Control">
 import os
 import sys
+# </editor-fold>
 
+
+# <editor-fold desc="Path control">
 HERE = os.path.dirname(os.path.abspath(__file__))
-
 sys.path.insert(0, HERE)
+# </editor-fold>
+
+
+# <editor-fold desc="PyTest Config">
+# def pytest_configure(config):
+#     config.addinivalue_line(
+#         "markers", "env(name): mark test to run only on named environment"
+#     )
+# </editor-fold>
